17c85557a10ef18061db76f8204c6b3508778572
userdefined <userdefined64@gmail.com>
2020-06-15 20:59:27 +0530

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,14 @@
+class Question{
+  String questionText;
+  bool questionAnswer;
+
+  Question(String q,bool a){
+    questionText=q;
+    questionAnswer=a;
+
+    //or simplified version is
+    //Question(this.questionText,this.questionAnswer);
+
+  }
+}
+
